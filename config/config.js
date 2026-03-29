e559d42dc8fd7885183ec063114ad323a42c7ab8
frf100191-ops <frf100191@gmail.com>
2026-03-29 23:52:51 +0530

    Update config.js

--- config/config.js
+++ config/config.js
@@ -8,17 +8,23 @@
  */
 
 const CONFIG = {
-  // Bot invite link - users will be redirected here when clicking "Invite Kali Billi"
-  botInviteUrl: "https://discord.com/api/oauth2/authorize?client_id=https://kali-billi-landing-page.vercel.app/&permissions=8&scope=bot",
+  // Kali Billi Bot invite link - users will be redirected here when clicking "Invite Bot"
+  botInviteUrl: "https://discord.com/api/oauth2/authorize?client_id=1485896423848284240&permissions=8&scope=bot",
+  
+  // CupidX Bot invite link - users will be redirected here when clicking "Invite CupidX"
+  cupidXInviteUrl: "https://discord.com/api/oauth2/authorize?client_id=1461333665811271723&permissions=8&scope=bot",
+  
+  // Security X Bot invite link - users will be redirected here when clicking "Invite Security X"
+  securityXInviteUrl: "https://discord.com/api/oauth2/authorize?client_id=1485896423848284240&permissions=8&scope=bot",
   
   // Support server invite link - users will be redirected here for support
   supportServerUrl: "https://discord.gg/UmY8mMewA4",
   
   // Bot name (for display)
-  botName: "Security X",
+  botName: "CupidX Development",
   
   // Bot description
-  botDescription: "Your Global Discord Security System - Antinuke, Automod and Moderation for your Discord server."
+  botDescription: "Support server for CupidX and Security X discord bots"
 };
 
 // Export for use in browsers
