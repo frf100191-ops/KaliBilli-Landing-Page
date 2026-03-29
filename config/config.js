1c14ebad11d438ea46c8d59813b14faf86d8edca
frf100191-ops <frf100191@gmail.com>
2026-03-29 23:29:34 +0530

    Update config.js

--- config/config.js
+++ config/config.js
@@ -9,16 +9,16 @@
 
 const CONFIG = {
   // Bot invite link - users will be redirected here when clicking "Invite Kali Billi"
-  botInviteUrl: "https://discord.com/api/oauth2/authorize?client_id=YOUR_BOT_CLIENT_ID&permissions=8&scope=bot",
+  botInviteUrl: "https://discord.com/api/oauth2/authorize?client_id=https://kali-billi-landing-page.vercel.app/&permissions=8&scope=bot",
   
   // Support server invite link - users will be redirected here for support
-  supportServerUrl: "https://discord.gg/YOUR_SERVER_INVITE_CODE",
+  supportServerUrl: "https://discord.gg/UmY8mMewA4",
   
   // Bot name (for display)
-  botName: "Kali Billi",
+  botName: "Security X",
   
   // Bot description
-  botDescription: "Your Cute Discord Companion - Fun, moderation, and AI chat for your Discord server."
+  botDescription: "Your Global Discord Security System - Antinuke, Automod and Moderation for your Discord server."
 };
 
 // Export for use in browsers
